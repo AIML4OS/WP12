80df5302dc933fbede0ee3b740b146273fbefce5
= <onyxia@vscode-python-245110-0.vscode-python-245110.projet-wp12-url-info-extraction.svc.cluster.local>
2025-06-18 13:15:11 +0000

    update code, not clean

--- web_corner/llm_api.py
+++ web_corner/llm_api.py
@@ -1,25 +1,22 @@
 import openai
 
 def LLM_API(api_key, input_config = None, model="magistral:24b", message_content=""):
-    if len(message_content) < 1:
-        error("Message should have content")
-
     client = openai.OpenAI(
         api_key = api_key,
         base_url = "https://llm.lab.sspcloud.fr/api"
     )
 
-    prompt_content = f"{message_content}, does the above-mentioned text, relating to a web-page, contain a job vacancy posting?"
+    prompt_content = f"{message_content}, does the above-mentioned text, relating to a web-page, contain a job vacancy posting OR link to a page containing job vacancies?"
 
     response = client.chat.completions.create(
         model = "magistral:24b",
         messages = [
-            {"role":"system", "content":"You are an expert about classifying texts, you always give exlusively Yes or No answers, one-word responses."},
+            {"role":"system", "max_completion_tokens":"1", "content":"You are an expert about classifying texts, your answer is a one-word response, chosen from [Yes, No]."},
             {"role":"user", "content": prompt_content},
         ],
-        temperature = 0.2,
-        max_tokens = 2,
-        top_p = 0.9
+        temperature = 0.0,
+        max_completion_tokens = 1,
+        max_tokens = 1
     )
 
     return response.choices[0].message.content
--- web_corner/main.py
+++ web_corner/main.py
@@ -1,27 +1,65 @@
 import requests
 import justext
 from llm_api import LLM_API
+import pandas as pd 
+import connect_storage
+import os
+import s3fs
+import json
+import langcodes
+import random
+
+# Create filesystem object
+fs = s3fs.S3FileSystem(
+    client_kwargs={'endpoint_url': 'https://'+'minio.lab.sspcloud.fr'},
+    key = os.environ["AWS_ACCESS_KEY_ID"], 
+    secret = os.environ["AWS_SECRET_ACCESS_KEY"], 
+    token = os.environ["AWS_SESSION_TOKEN"])
+BUCKET = "projet-wp12-url-info-extraction"
+FILE_KEY_S3 = "urlsWINChallenge.csv"
+FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
+
+with fs.open(FILE_PATH_S3, mode="rb") as file_in:
+    url_data = pd.read_csv(file_in, sep=",")
 
 with open("web_corner/api_key.txt", "r", encoding="utf-8") as file:
     api_key = file.readlines()[0]
 
-urls = ... # TODO read urls
-urls = [
-    "https://www.cbs.nl/nl-nl/vacature/toolexpert/1ae1a790f9284748aca2b34da8cae607",
-    "https://www.cbs.nl/nl-nl/visualisaties/dashboard-consumentenprijzen"
-]
+url_country = list(zip(url_data['url'], url_data['country']))
+random.shuffle(url_country)
+url_country = url_country[:500]
 
 job_vacancies = {}
-for url in urls:
-    response = requests.get(url)
+for url, country in url_country:
+    try:
+        response = requests.get(url)
+    except:
+        continue
     # TODO determine language 
-    extracted_paragraphs = justext.justext(response.content, justext.get_stoplist("Dutch"))
+
+    language = "English"
+    if country == "DE":
+        language = "German"
+    elif country == "NL":
+        language = "Dutch"
+    elif country == "PL":
+        language = "Polish"
+    elif country == "AT":
+        country = "German"
+
+    extracted_paragraphs = justext.justext(response.content, justext.get_stoplist(language))
     extracted_text = "".join([paragraph.text for paragraph in extracted_paragraphs if not paragraph.is_boilerplate])
 
+    if len(extracted_text) < 1:
+        continue
     # TODO get prompt from config
     response = LLM_API(api_key, None, "magistral_24b", extracted_text)
     job_vacancies[url] = response
 
-print(job_vacancies)
+
+print("# of items that are too long")
+print(len([v for v in job_vacancies.values() if len(v) > 3]))
 
 
+with open("web_corner/output.json", "w") as fp:
+  json.dump(job_vacancies , fp)
