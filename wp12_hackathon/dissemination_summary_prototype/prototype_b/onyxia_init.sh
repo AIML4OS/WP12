6da3c395a479ee373b2feac632705a2ba059f774
ialvata <ivo.a.v.tavares@gmail.com>
2025-07-08 18:55:43 +0100

    updating onyxia init script

--- wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
+++ wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
@@ -1,1 +1,10 @@
 git clone https://github.com/AIML4OS/WP12.git
+cd WP12/wp12_hackathon/dissemination_summary_prototype/prototype_b/
+python3 -m venv .venv_pdf
+source .venv_pdf/bin/activate
+pip install -r requirements.txt
+
+# Ollama install
+curl -fsSL https://ollama.com/install.sh | sh
+# ollama serve
+# bash ollama run llama3.2
