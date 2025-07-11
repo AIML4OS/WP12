232917bb9960ef5f0a7518c9f11db37d50eed89c
Ivo_Tavares <110241614+ialvata@users.noreply.github.com>
2025-07-11 18:29:22 +0100

    Update onyxia_init.sh

--- wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
+++ wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/AIML4OS/WP12.git
 cd WP12/wp12_hackathon/dissemination_summary_prototype/prototype_b/
 python3 -m venv .venv_pdf
-source .venv_pdf/bin/activate
-pip install -r requirements.txt
+# source .venv_pdf/bin/activate
+# pip install -r requirements.txt
 
 # Ollama install
 curl -fsSL https://ollama.com/install.sh | sh
