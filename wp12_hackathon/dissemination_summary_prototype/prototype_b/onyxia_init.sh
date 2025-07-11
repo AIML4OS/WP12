53018c90402b45747ad77aad1ff366802f7e19ef
Ivo_Tavares <110241614+ialvata@users.noreply.github.com>
2025-07-11 23:44:17 +0100

    Update onyxia_init.sh

--- wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
+++ wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
@@ -2,5 +2,4 @@
 git clone https://github.com/AIML4OS/WP12.git
 cd WP12/wp12_hackathon/dissemination_summary_prototype/prototype_b/
 chmod 755 launch_app set_ollama.sh set_venv.sh
-
-# ln -s launch_app.sh /usr/local/bin/launch_app
+ln -s launch_app /usr/local/bin/launch_app
