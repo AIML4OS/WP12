fd387d7e0e059aefc0523818df1b782786179afd
ialvata <ivo.a.v.tavares@gmail.com>
2025-07-11 21:24:16 +0100

    updating scripts

--- wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
+++ wp12_hackathon/dissemination_summary_prototype/prototype_b/onyxia_init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
 git clone https://github.com/AIML4OS/WP12.git
 cd WP12/wp12_hackathon/dissemination_summary_prototype/prototype_b/
+chmod 755 launch_app set_ollama.sh set_venv.sh
+
+# ln -s launch_app.sh /usr/local/bin/launch_app
